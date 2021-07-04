775820c09c78e017238d0fefb8b00c9c919555a2
mneururer <michalis.neururer@gmail.com>
2021-07-04 12:31:58 +0200

    First code commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from "react";
+
 
 function App() {
+  const [name, setName] = useState("");
+  const [ETHAddress, setETHAddress] = useState("")
+  var regex_first_occurrence = new RegExp('0x[a-fA-F0-9]{40}(?![a-fA-F0-9])');  
+  var regex_all_occurrences = new RegExp('0x[a-fA-F0-9]{40}(?![a-fA-F0-9])', 'g');  
+
+  function handleNameChange(e) {
+    setName(e.target.value);
+
+  }
+  function findFirstEthereumAddress(description) {
+    var address;
+    address = description.match(regex_first_occurrence);
+    return address
+  }
+  function findAllEthereumAddress(description) {
+    var addresses;
+    addresses = description.match(regex_all_occurrences);
+    addresses = addresses ? addresses.join('\n') : "no match";
+    return addresses
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <body className="App-header">
+      <textarea
+        type="text"
+        pattern="[0-9]"
+        id="eth-address-first-occurrence"
+        className="textarea input__lg"
+        name="eth_adress"
+        autoComplete="on"
+        style={{width:"20%"}}
+        onChange={handleNameChange}
+      />
+      Matching first occurrence:
+      <p> {findFirstEthereumAddress(name)} </p>
+      Match all occurrences:
+      <p> {findAllEthereumAddress(name)} </p>
+
+      </body>
+
     </div>
   );
 }
